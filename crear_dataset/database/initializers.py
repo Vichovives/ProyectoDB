7a332f336fdbc271048e5ed8ab4a951a9f6bf1d0
Jorge Espinosa <65792762+JorgeEspinosaV@users.noreply.github.com>
2023-11-05 18:16:24 -0300

    Update initializers.py

--- crear_dataset/database/initializers.py
+++ crear_dataset/database/initializers.py
@@ -1,16 +1,18 @@
 # Conexion a DB
 import psycopg2
 
+"""
 def DBInstance():
     try:
         conn = psycopg2.connect(
-            host="db-postgresql-nyc3-82326-do-user-14766642-0.c.db.ondigitalocean.com",
-            database="defaultdb",
-            user="doadmin",
-            password="AVNS_GjSOTWxWLvet9bAlVOL",
-            port=25060
+            host=host,
+            database=database,
+            user=user,
+            password=password,
+            port=port
         )
         return conn
     except psycopg2.Error as err:
         print("Error al conectar la base de datos: ", err)
         return None
+"""
